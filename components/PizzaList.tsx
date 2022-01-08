1e63175b3d58101762b46112b67b925cafeb77a1
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-01-08 22:15:10 +0900

    update

---
+++ components/PizzaList.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/PizzaList.module.scss'
+
+const PizzaList = () => {
+  return (
+    <div className={styles.container}>
+      <h1>THE BEST PIZZA IN TOWN</h1>
+    </div>
+  )
+}
+
+export default PizzaList
