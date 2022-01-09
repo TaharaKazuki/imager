99ad2eecccce56d2be80226d88a1628207ebe996
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-01-09 10:51:04 +0900

    update

--- components/PizzaList.tsx
+++ components/PizzaList.tsx
@@ -1,9 +1,18 @@
 import styles from '../styles/PizzaList.module.scss'
+import PizzaCard from './PizzaCard'
 
 const PizzaList = () => {
+  const renderCard = [...Array(10)].map((_, i) => <PizzaCard key={i} />)
+
   return (
     <div className={styles.container}>
-      <h1>THE BEST PIZZA IN TOWN</h1>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit arcu
+        in pretium molestie. Interdum et malesuada fames acme. Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit.
+      </p>
+      <div className={styles.wrapper}>{renderCard}</div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Featured from '../components/Featured'
+import PizzaList from '../components/PizzaList'
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
+      <PizzaList />
     </div>
   )
 }
